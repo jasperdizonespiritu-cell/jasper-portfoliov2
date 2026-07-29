b96b1e2945e2a3a0bb6dc9f2c2437f576d1d124c
jasperdizonespiritu-cell <jasperdizonespiritu@gmail.com>
2026-07-30 06:31:52 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
+const titles = [
+    "Executive Assistant",
+    "Virtual Assistant",
+    "Customer Support Specialist",
+    "Workflow Automation Enthusiast",
+    "GoHighLevel CRM Support"
+];
 
+let titleIndex = 0;
+let charIndex = 0;
+let deleting = false;
+
+const heading = document.querySelector(".hero h2");
+
+function typeEffect() {
+
+    const current = titles[titleIndex];
+
+    if (!deleting) {
+
+        heading.textContent = current.substring(0, charIndex++);
+
+        if (charIndex > current.length) {
+            deleting = true;
+            setTimeout(typeEffect, 1500);
+            return;
+        }
+
+    } else {
+
+        heading.textContent = current.substring(0, charIndex--);
+
+        if (charIndex < 0) {
+            deleting = false;
+            titleIndex = (titleIndex + 1) % titles.length;
+        }
+
+    }
+
+    setTimeout(typeEffect, deleting ? 50 : 100);
+
+}
+
+typeEffect();
